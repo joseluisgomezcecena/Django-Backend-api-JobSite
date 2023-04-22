185b896a2e7c87d167e0587de64c87b76a37c120
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2023-04-21 17:48:55 -0700

    Added CRUD along with Api routes.

--- job/urls.py
+++ job/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('jobs/', views.getAllJobs, name="jobs"),
+    path('jobs/<str:pk>/', views.getJob, name="job"),
+    path('jobs/new/', views.createJob, name="create-job"),
+    path('jobs/update/<str:pk>/', views.updateJob, name="update-job"),
+    path('jobs/delete/<str:pk>/', views.deleteJob, name="delete-job"),
 ]
--- job/views.py
+++ job/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Job
 from .serializers import JobSerializer
+
+from django.shortcuts import get_list_or_404, get_object_or_404
 # Create your views here.
 
 
@@ -14,3 +16,76 @@ def getAllJobs(request):
 
     serializer = JobSerializer(jobs, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getJob(request, pk):
+
+    #job = Job.objects.get(id=pk)
+    job = get_object_or_404(Job, id=pk)
+
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def createJob(request):
+
+    data = request.data
+
+    job = Job.objects.create(**data)
+
+    """
+    job = Job.objects.create(
+        title=data['title'],
+        company=data['company'],
+        description=data['description'],
+        salary=data['salary'],
+        location=data['location'],
+        email=data['email'],
+        phone=data['phone'],
+        url=data['url'],
+        date=data['date'],
+    )
+    """
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+
+
+@api_view(['PUT'])
+def updateJob(request, pk):
+
+    job = get_object_or_404(Job, id=pk)
+
+    job.title = request.data['title']
+    job.description = request.data['description']
+    job.email = request.data['email']
+    job.address = request.data['address']
+    job.job_type = request.data['job_type']
+    job.education = request.data['education']
+    job.experience = request.data['experience']
+    job.industry = request.data['industry']
+    job.salary = request.data['salary']
+    job.positions = request.data['positions']
+    job.company = request.data['company']
+    job.city = request.data['city']
+    job.state = request.data['state']
+    job.country = request.data['country']
+    #job.last_date = request.data['last_date']
+    #job.point = request.data['point']
+    #job.user = request.data['user']
+
+    job.save()
+
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteJob(request, pk):
+
+    job = get_object_or_404(Job, id=pk)
+    job.delete()
+
+    return Response({'message': 'Job deleted successfully'})
