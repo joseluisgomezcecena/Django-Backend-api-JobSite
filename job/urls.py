3275a0a203370106892e60c2b6e3f456dd9657a2
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2023-04-24 09:09:52 -0700

    Added getTopicStats to routes.

--- job/urls.py
+++ job/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('jobs/new/', views.createJob, name="create-job"),
     path('jobs/update/<str:pk>/', views.updateJob, name="update-job"),
     path('jobs/delete/<str:pk>/', views.deleteJob, name="delete-job"),
+
+    path('stats/<str:topic>/', views.getTopicStats, name="stats"),
 ]
